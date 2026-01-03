b03139f1f293384ad7d3c8ed8346ed07cd6f71c9
HenrBmp <henriquerodr20@gmail.com>
2026-01-03 17:14:26 -0300

    transferi ocodigo js para apenas 1 arquivo

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-import { FORECAST } from "./scripts/getForecast.js";
+navigator.geolocation.getCurrentPosition(
+    async function success(position) {
+        // OBTENDO A PREVISÃO DO TEMPO
+        const LAT = position.coords.latitude;
+        const LON = position.coords.longitude;
+        const API_KEY = "2baa7ddf3cd92dc580ddb29560a1e03c";
+        const API_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${API_KEY}&units=metric`;
+        
+        const REQ = await fetch(API_WEATHER_URL);
+        const DATA = await REQ.json();
+        
+        // APLICANDO INFORMAÇÕES NA TELA
+        const CITY = document.getElementById("city-name");
+        const TEMP = document.getElementById("temperatura");
 
-const DATA = await FORECAST;
+        CITY.innerHTML = DATA.name;
+        TEMP.innerHTML = DATA.main.temp;
+    },
+    function error(err) {
+        // TRATAMENTO DE ERRO
+        // PARA DEPOIS
+    }
+)
