cf5ea4c569a3ffd920c50b2230911485d81f6e36
HenrBmp <henriquerodr20@gmail.com>
2026-01-10 22:36:48 -0300

    adicionei icones de minima e maxima

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ navigator.geolocation.getCurrentPosition(
         
         const REQ = await fetch(API_WEATHER_URL);
         const DATA = await REQ.json();
+        console.log(DATA);
         
         // CRIANDO OS ELEMENTOS DA PAGINA
         const CITY = document.getElementById("city-name");
@@ -17,12 +18,29 @@ navigator.geolocation.getCurrentPosition(
         const MAX_TEMP = document.getElementById("temp-maxima");
         const FEELSLIKE = document.getElementById("sensacao");
         const ICON = document.getElementById("forecast-icon");
+
         const DESC = document.getElementById("descricao");
         const DESC_EN = DATA.weather[0].description;
 
-        // TRADUZINDO DESCRICAO
-        // const res = await 
-        const DESC_PT = "placeholder";
+        const DESC_PT = await translateText(DESC_EN);
+
+        // SETA PARA A TEMPERATURA MINIMA
+        const DOWN_ARROW = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        DOWN_ARROW.setAttribute("viewBox", "0 -960 960 960");
+        const DOWN_ARROW_PATH = document.createElement("path");
+        DOWN_ARROW_PATH.setAttribute("d", "M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z");
+        DOWN_ARROW.appendChild(DOWN_ARROW_PATH);
+
+        // SETA PARA A TEMPERATURA MAX
+        const UP_ARROW = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        UP_ARROW.setAttribute("viewBox", "0 -960 960 960");
+        const UP_ARROW_PATH = document.createElement("path");
+        UP_ARROW_PATH.setAttribute("d", "M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z");
+        UP_ARROW.appendChild(UP_ARROW_PATH);
+
+        [DOWN_ARROW, UP_ARROW].forEach(
+            e => e.classList.add("svg_arrow")
+        );
         
         // REMOVENDO O CARREGAMENTO
         document.getElementById("loader").remove();
@@ -30,13 +48,21 @@ navigator.geolocation.getCurrentPosition(
         // APLICANDO INFORMAÇÕES NA TELA
         CITY.innerHTML = DATA.name;
         TEMP.innerHTML = DATA.main.temp;
-        MIN_TEMP.innerHTML = DATA.main.temp_min;
-        MAX_TEMP.innerHTML = DATA.main.temp_max;
+        MIN_TEMP.appendChild(DOWN_ARROW);
+        MIN_TEMP.innerHTML += DATA.main.temp_min;
+        MAX_TEMP.appendChild(UP_ARROW);
+        MAX_TEMP.innerHTML += DATA.main.temp_max;
         FEELSLIKE.innerHTML = DATA.main.feels_like;
         ICON.setAttribute("src", `https://openweathermap.org/img/wn/${DATA.weather[0].icon}.png`);
-        DESC.innerHTML = DESC_PT;
+        DESC.innerHTML = DESC_PT ? DESC_PT : DESC_EN;
+        const TEMP_ELEMENTS = document.getElementsByClassName("celsius");
+        console.log(TEMP_ELEMENTS);
+        for (let i = 0 ; i < TEMP_ELEMENTS.length ; i++) {
+            TEMP_ELEMENTS[i].innerHTML += "ºC";
+        };
     },
     function error(err) {
+        console.log(err)
         // TRATAMENTO DE ERRO
         // PARA DEPOIS
     }
